9820310dd1ffafd507e35078b87f72871c45f814
imshashanksharma <shashanksharma7856@gmail.com>
2025-10-06 01:11:18 +0530

    final excelidraw

--- draw-app/apps/http-backend/src/index.ts
+++ draw-app/apps/http-backend/src/index.ts
@@ -1,18 +1,22 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middleware";
 import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
-import { JWT_SECRET } from "@repo/backend-common/config";
 import { prismaClient } from "@repo/db/client";
+import cors from "cors";
+
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.post("/signup", async (req, res) => {
+
     const parsedData = CreateUserSchema.safeParse(req.body);
     if (!parsedData.success) {
         console.log(parsedData.error);
         res.json({
-            message : "Incorrect inputs"
+            message: "Incorrect inputs"
         })
         return;
     }
@@ -20,16 +24,17 @@ app.post("/signup", async (req, res) => {
         const user = await prismaClient.user.create({
             data: {
                 email: parsedData.data?.username,
-                password: parsedData.data?.password,
-                name: parsedData.data?.name
+                // TODO: Hash the pw
+                password: parsedData.data.password,
+                name: parsedData.data.name
             }
         })
         res.json({
-            userId : user.id
+            userId: user.id
         })
     } catch (e) {
         res.status(411).json({
-            message : "User already exists with this username"
+            message: "User already exists with this username"
         })
     }
 })
@@ -67,7 +72,6 @@ app.post("/signin", async (req, res) => {
     })
 })
 
-
 app.post("/room", middleware, async (req, res) => {
     const parsedData = CreateRoomSchema.safeParse(req.body);
     if (!parsedData.success) {
@@ -96,6 +100,7 @@ app.post("/room", middleware, async (req, res) => {
         })
     }
 })
+
 app.get("/chats/:roomId", async (req, res) => {
     try {
         const roomId = Number(req.params.roomId);
@@ -122,7 +127,6 @@ app.get("/chats/:roomId", async (req, res) => {
 
 })
 
-
 app.get("/room/:slug", async (req, res) => {
     const slug = req.params.slug;
     const room = await prismaClient.room.findFirst({
@@ -135,6 +139,5 @@ app.get("/room/:slug", async (req, res) => {
         room
     })
 })
-
 
 app.listen(3001);
